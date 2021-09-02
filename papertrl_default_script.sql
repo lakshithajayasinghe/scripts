a9dff55c0f20851666b1de91dd04de18df2514c4
pptllakshitha <lakshitha.j@papertrl.com>
2021-09-02 06:00:48 +0000

    default-script updated

--- papertrl_default_script.sql
+++ papertrl_default_script.sql
@@ -14,56 +14,6 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `Persons1`
---
-
-DROP TABLE IF EXISTS `Persons1`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Persons1` (
-  `PersonID` int(11) DEFAULT NULL,
-  `LastName` varchar(255) DEFAULT NULL,
-  `FirstName` varchar(255) DEFAULT NULL,
-  `Address` varchar(255) DEFAULT NULL,
-  `City` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Persons1`
---
-
-LOCK TABLES `Persons1` WRITE;
-/*!40000 ALTER TABLE `Persons1` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Persons1` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Persons2`
---
-
-DROP TABLE IF EXISTS `Persons2`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Persons2` (
-  `PersonID` int(11) DEFAULT NULL,
-  `LastName` varchar(255) DEFAULT NULL,
-  `FirstName` varchar(255) DEFAULT NULL,
-  `Address` varchar(255) DEFAULT NULL,
-  `City` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Persons2`
---
-
-LOCK TABLES `Persons2` WRITE;
-/*!40000 ALTER TABLE `Persons2` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Persons2` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -74,4 +24,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-09-01 10:51:48
+-- Dump completed on 2021-09-02  6:00:48
